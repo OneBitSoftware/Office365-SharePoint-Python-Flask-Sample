55a4c4e2c527f5e0c7f19c6547d32357c951c702
Velin <velin.georgiev@gmail.com>
2016-02-09 01:01:08 +0200

    comments added to the app

--- src/office365flask/app.py
+++ src/office365flask/app.py
@@ -1,40 +1,60 @@
 import os
 from flask import Flask, request, redirect, session, render_template, url_for, flash
-from office365 import login_url, tenant_url, access_token, upload_file
+from office365 import login_url, issuance_url, access_token, upload_file
 
+# Init the flask app.
 app = Flask(__name__)
+
+# Load confuguration variables.
 app.config.from_pyfile('app.cfg')
 c = app.config
 
 # Make the WSGI interface available at the top level so wfastcgi can get it.
+# The WSGI interface is needed for Azure deployment. 
 wsgi_app = app.wsgi_app
 
+# Routes and views for the flask application.
 @app.route('/')
 def home(): 
+    # Renders the home page. 
     redirect_uri = '{0}auth'.format(request.host_url)
+    # Generates Azure AD authorization endpoint url with parameters so the user authenticates and consents, if consent is required.
     url = login_url(redirect_uri, c['CLIENT_ID'], c['RESOURCE'], c['AUTHORITY'])
+    # Renders the index template with additional prams for the login url and a bool is_authenticated if access token is stored in session.
     return render_template('index.html', url=url, is_authenticated='access_token' in session)
 
 @app.route('/auth', methods=['POST'])
 def auth():
+    # Handles the Azure AD authorization endpoint response and sends second repsonse to get access token. 
+    # Gets the token_id from the flask response form dictionary.
     token_id = request.form['id_token']
+    # Gets the authorization code from the flask response form dictionary.
     code = request.form['code']
+    # Constructs redirect uri to be send as query string param to Azure AD token issuance endpoint.
     redirect_uri = '{0}auth'.format(request.host_url)
-    sharepoint_url = tenant_url(token_id, c['AUTHORITY'])
-    session['access_token'] = access_token(sharepoint_url, redirect_uri, c['CLIENT_ID'], code, c['CLIENT_SECRET'])
+    # Constructs Azure AD token issuance endpoint url.
+    url = issuance_url(token_id, c['AUTHORITY'])
+    # Requests access token and stores it in session.
+    session['access_token'] = access_token(url, redirect_uri, c['CLIENT_ID'], code, c['CLIENT_SECRET'])
     return redirect(url_for('home'))
 
 @app.route('/upload', methods=['POST'])
 def upload():
+    # Uploads file to SharePoint Tenant.
     if 'access_token' in session:
+        # File from the submitted upload form.
         file = request.files['file']
+        # Sends request to SharePoint api with the file.
         success = upload_file(c['RESOURCE'], file.filename, file.stream.read(), session['access_token'])
         if success:
+            # Success flash message if the file is successfully uploaded.
             flash('You successfully uploaded file {0}'.format(file.filename))
         else:
             flash('Could not upload file {0}'.format(file.filename))
     return redirect(url_for('home'))
 
+
+# This script runs the application using a development server.
 if __name__ == '__main__':
     HOST = os.environ.get('SERVER_HOST', 'localhost')
     try:
--- src/office365flask/office365.py
+++ src/office365flask/office365.py
@@ -2,29 +2,40 @@ import uuid, jwt, requests
 from six.moves.urllib.parse import quote
 
 def login_url(redirect_uri, client_id, resource, authority):
+    # Generates Azure AD authorization endpoint url with parameters so the user authenticates and consents, if consent is required.
     params = '?client_id='+client_id
     params += '&redirect_uri='+quote(redirect_uri)
+    # Will request id_token and also authorization code.
     params += '&response_type=code+id_token'
     params += '&scope=openid'
     params += '&nonce='+str(uuid.uuid4())
     params += '&response_mode=form_post'
     params += '&resource='+quote(resource)
+    # Azure AD authorization endpoint url.
     return '{0}/common/oauth2/authorize{1}'.format(authority, params)
 
-def tenant_url(id_token, authority):
+def issuance_url(id_token, authority):
+    # id_token is JSON Web Token and it can be decoded with PyJWT.
+    # Extracting 'tid' from the decoded id_token dictionary would provide the tenant id.
     tenant_id = jwt.decode(id_token, verify=False)['tid'] 
+    # The tenant id is used to construct the Azure AD token issuance endpoint url.
     return '{0}/{1}/oauth2/token'.format(authority, tenant_id)
 
-def access_token(tenant_url, redirect_uri, client_id, code, client_secret):
+def access_token(issuance_url, redirect_uri, client_id, code, client_secret):
+    # Initialize access token request payload
     data = { 'client_id': client_id,
              'client_secret': client_secret,
              'grant_type': 'authorization_code',
              'code': code,
              'redirect_uri': redirect_uri }
-    return requests.post(tenant_url, data=data).json()['access_token']
+    # Sends post request to the Azure AD token issuance endpoint with the payload included in the body.
+    # Access token to be aquired on successful response.
+    return requests.post(issuance_url, data=data).json()['access_token']
 
-def upload_file(url, fname, file, token):
+def upload_file(url, fname, file, access_token):
+    # Sets request headers with the access token included
     headers = { 'Content-Type':'application/json',
-                'Authorization': 'Bearer {0}'.format(token) }
+                'Authorization': 'Bearer {0}'.format(access_token) }
+    # Post requrest to the SharePoint api with OData query for document upload.
     r = requests.post("{0}/_api/web/lists/getbytitle('Documents')/rootfolder/files/add(url='{1}', overwrite=true)".format(url, fname), data=file, headers=headers)
     return r.status_code == 200
